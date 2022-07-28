f91c9c841c950e107952cfcf791bd014cf2c9f7f
MathMendesReis <matheusmendesreis@outlook.com>
2022-07-28 18:42:43 -0300

    Exercios resolvidos

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,1 +1,15 @@
 const minhaString = "Eu não sou supersticioso, mas sou um pouco ________.      ";
+const num = [1, 3 ,-1, 0, -20, 7, 10];
+const strings = ["matheus", "chocolate", "medo", "array"];
+const misto = ["sorte",2,true];0
+
+console.log("quantidade de itens: "+minhaString.length);
+console.log("quantidade de itens: "+num.length);
+console.log("quantidade de itens: "+strings.length);
+console.log("quantidade de itens: "+misto.length);
+console.log(" Imprima o primeiro item do primeiro array:"+num[0])
+console.log(" Imprima o segundo item do segundo array: "+strings[1])
+console.log(" Imprima o terceiro item do terceiro array: "+misto[2])
+
+console.log(num.includes(1))
+console.log(misto.includes("menor"))
