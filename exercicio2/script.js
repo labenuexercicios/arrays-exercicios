d066f4b8c1b6db8037974f2b49c6caf6bb500583
Daniel <daniel.sprcp@gmail.com>
2022-07-28 16:35:10 -0300

    Exercício 2 finalizado

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,1 +1,18 @@
-const minhaString = "Eu não sou supersticioso, mas sou um pouco ________.      ";
+// EXERCICIO 2:
+
+// IMPRIMINDO INFORMAÇÕES SOBRE AS ARRAYS:
+
+const arrayNumbers = [2, 9, 6, 1, 4, 5]
+const arrayStrings = ["uva", "goiaba", "banana", "maçã", "kiwi","ameixa", "abacaxi"]
+const arrayMisto = [2, "banana", true, "uva", false, 5, 1]
+
+console.log(arrayNumbers.lenght)
+console.log(arrayStrings.lenght)
+console.log(arrayMisto.lenght)
+
+console.log(arrayNumbers[0])
+console.log(arrayStrings[1])
+console.log(arrayMisto[2])
+
+console.log(arrayNumbers.includes(9))
+console.log(arrayStrings.includes("morango"))
