ee68fccbaf888f82f60dc43004ea51f6f4c24add
Lorennaborges22 <loreborges1998@gmail.com>
2022-07-27 22:27:37 -0300

    array

---
+++ exercicio1/script.js
@@ -0,0 +1,24 @@
+const numeros = [5,3,8,6,0]
+console.log(numeros)
+
+const listaCor = ["azul","verde","rosa"]
+console.log(listaCor)
+
+const mistura = [22,"livros",false]
+console.log(mistura)
+
+// exercicio2
+console.log(numeros.length)
+console.log(listaCor.length)
+console.log(mistura.length)
+
+console.log(numeros[0])
+console.log(listaCor[1])
+console.log(mistura[2])
+
+console.log(numeros.includes(8))
+console.log(mistura.includes(8))
+console.log(mistura.includes(22))
+
+
+
---
+++ exercicio3/script.js
@@ -0,0 +1,18 @@
+const numero = [3,5,7,2,9,0]
+const objetos = ["caneta","lápis","celular"]
+const cor = ["vermelho","azul","preto"]
+
+const numeroCopia = numero.push(10)
+console.log(numero)
+//console.log(numeroCopia) TESTE
+
+const objetosCopia = objetos.pop()
+console.log(objetos)
+//console.log(objetosCopia) TESTE
+
+
+const corCopia = cor.splice(1,1)
+console.log(cor)
+
+
+
