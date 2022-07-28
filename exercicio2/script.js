99a42f01cc43807674d55b2de57ee276c17135c9
roni <ronildo.sad2@hotmail.com>
2022-07-28 20:22:49 -0300

    terminando exercicio array

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,1 +1,21 @@
-const minhaString = "Eu não sou supersticioso, mas sou um pouco ________.      ";
+// const minhaString = "Eu não sou supersticioso, mas sou um pouco ________.      "
+
+
+
+let arrayNumberos = [ 7, 9, 1,]
+let arrayString = [ "arroz", "feijao", "batata"]
+let arrayTodos= [ "doce",  3, false]
+
+
+
+console.log("Tamanho do primeiro array:", arrayNumberos.length)
+console.log("Tamanho do segundo array:",arrayString.length)
+console.log("Tamanho do terceiro array:",arrayTodos.length)
+
+console.log("Primeiro item:", arrayNumberos[0])
+console.log("Segundo item:", arrayString[1])
+console.log("Terceiro item:", arrayTodos[2])
+
+
+console.log("Existe 2 no primeiro array?", arrayNumberos.includes(2))
+console.log("Existe 3 no terceiro array?", arrayTodos.includes(3))
