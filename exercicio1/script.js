681ee973a193a1dddbb65c04d8a80e01cbf758bf
LABNUISA <araujo_isaias@outlook.com>
2022-07-28 01:00:49 -0300

    pratica 010 entregue

---
+++ exercicio1/script.js
@@ -0,0 +1,9 @@
+//CRIAÇÃO
+
+const numero = [8, 6, 9, 1]
+console.log(numero)
+const string = ['marcos', 'jose', 'carmem']
+console.log(string)
+const mista = [15, 'paula', true]
+console.log(mista)
+
