abdfc4a40d0498b7ccee22ca5788e27ab52403e7
Gabriel Max <gabrielmaxcerqueira@gmail.com>
2022-07-28 16:39:34 -0300

    Arrays A10 Exercicios

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,1 +1,26 @@
-const minhaString = "Eu não sou supersticioso, mas sou um pouco ________.      ";
+// const minhaString = "Eu não sou supersticioso, mas sou um pouco ________.      ";
+
+//Exercicio 2
+
+const arrayNumeros = [2, 4, 8, 10]
+console.log(arrayNumeros)
+console.log(arrayNumeros.length)
+
+const arrayGames = ["Uncharted", "FallGuys", "Sekiro"];
+console.log(arrayGames)
+console.log(arrayGames.length)
+
+const arrayMisto = ["AmongUs", "DevilMayCry", 15, 20, true, false];
+console.log(arrayMisto)
+console.log(arrayMisto.length)
+
+//Precisa ser separado os consoles. 
+console.log(arrayNumeros[0]); 
+console.log(arrayGames[1]); 
+console.log(arrayMisto[2]);  
+
+console.log(arrayNumeros.includes(2))
+console.log(arrayMisto.includes(21))
+
+
+
