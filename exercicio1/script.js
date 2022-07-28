e0289ca0c2f90eb522515a8416248139b9cbc86f
nosdliw <108556065+nosdliw@users.noreply.github.com>
2022-07-27 21:15:06 -0300

    Atividade 1, ok

---
+++ exercicio1/script.js
@@ -0,0 +1,7 @@
+const arrayNumeros = [2, 23, 78, 33, 22]
+const arrayStrings = ["Array", "é legal", "só formular", "e fazer com calma"]
+const arratAleatorio = [6, "Sucesso", true]
+
+console.log (arrayNumeros)
+console.log (arrayStrings)
+console.log (arratAleatorio)
