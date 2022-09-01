2b59495325b0f53ec0ac9bb90a112a509967de08
Ana Marmitt <anap.mmarmitt@gmail.com>
2022-09-01 15:49:26 -0300

    ex2-feito

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,1 +1,12 @@
-const minhaString = "Eu não sou supersticioso, mas sou um pouco ________.      ";
+const arrayNum = [4, 8, 15, 16, 23, 42],
+  arrayString = ["luca", "fufuchan", "Mysta"],
+  arrayBabygirl = [1, true, "daemon", "babygirl"];
+
+console.log(arrayNum.length);
+console.log(arrayString.length);
+console.log(arrayBabygirl.length);
+
+console.log(arrayNum[0], arrayString[1], arrayBabygirl[2]);
+
+console.log(arrayNum.includes(23));
+console.log(arrayBabygirl.includes("wrong"));
