b4c7ea5aff87e109b743b8171199282bf718afbe
karoline254 <karolineexecut@gmail.com>
2022-09-01 11:11:43 -0300

    Exercicio01

---
+++ exercicio1/script.js
@@ -0,0 +1,6 @@
+const arrayNum = [27, 35, 12, 9, 30, 3, 14]
+const arrayNomes = ["Karoline", "Camila", "Catarina", "Cecilia", "Lucca"]
+const arrayMix = ["Hello", 18, false]
+console.log(arrayNum)
+console.log(arrayNomes)
+console.log(arrayMix)
