3cbcecc56be065ba478a5cd781bc5e11e8b61530
VictorOgataj8 <victor.ogataj8@gmail.com>
2022-09-01 16:00:29 -0300

    exercicio 2 feito

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,1 +1,9 @@
 const minhaString = "Eu não sou supersticioso, mas sou um pouco ________.      ";
+const arrayUm= [3, 2, 4, 5, ,8 ,1]
+const arrayDois=["bola", "boneca", "caminhao"]
+const arrayTres= [2, "triangulo", true]
+console.log(`arrayUm:${arrayUm[0]}
+arrayDois:${arrayDois[1]}
+arrayTres:${arrayTres[2]}`)
+console.log(arrayUm.includes(3))
+console.log(arrayTres.includes("quadrado"))
