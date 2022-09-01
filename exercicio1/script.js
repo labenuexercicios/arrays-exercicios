46036328219e9c5b3c3048d700d922d582d7a363
igorazuos <azuosigor@gmail.com>
2022-09-01 13:24:14 -0300

    atualização exercicio1 a010

---
+++ exercicio1/script.js
@@ -0,0 +1,7 @@
+const arrayNumero = [5,10,6,1,3]
+const arrayString = ["mãe", "pai", "avô"]
+const arrayBagunca = [5, "igor", false]
+
+console.log(arrayNumero)
+console.log(arrayString)
+console.log(arrayBagunca)
