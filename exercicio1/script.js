ff5fb089cd1006e111f0df4296661faaa5e01afd
brunomaschietto <bruno4_10@hotmail.com>
2022-09-01 15:51:06 -0300

    Exercicios resolvidos de Array

---
+++ exercicio1/script.js
@@ -0,0 +1,7 @@
+const arrayNumeros = [1, 5, 3, 6, 8]
+const arrayStrings = ['Bruno', 'Giovane', 'Marcelo', 'Elza']
+const arrayMesclado = ['Bruno', 12, 50, 'Maschietto', true]
+console.log(arrayNumeros);
+console.log(arrayStrings);
+console.log(arrayMesclado);
+
