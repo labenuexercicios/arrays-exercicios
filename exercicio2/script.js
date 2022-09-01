05f7fecf3b0f955e025ea32976406caac1fcb6ee
PedroMaiaF <pedromaiaf21@gmail.com>
2022-09-01 18:54:57 -0400

    Exercício 2-corrigido novamente

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -7,16 +7,25 @@ console.log(listaFinal)
 console.log(listaMxConvite.length)
 console.log(listaTotal.length)
 console.log(listaFinal.length)
-//- Imprima o primeiro item do primeiro array, o segundo item do segundo array, e o terceiro item do terceiro array;
+//- Imprima o primeiro item do primeiro array, 
+//o segundo item do segundo array, e o terceiro item do terceiro array;
+console.log("modo impressão em única variável:")
 console.log(listaMxConvite[0], listaTotal[1], listaFinal[2])
 //Outro modo de imprimir ex1:
+console.log("Modo impressão separada:")
 console.log(listaMxConvite[0])
 console.log(listaTotal[1])
 console.log(listaFinal[2])
 //Outro modo de imprimir ex2:
-console.log(listaMxConvite.splice(0,1))
-console.log(listaTotal.splice(1,1))
-console.log(listaFinal.splice(2,1))
+console.log("modo slice:")
+console.log(listaMxConvite.slice(0,1))
+console.log(listaTotal.slice(1,2))
+console.log(listaFinal.slice(2,3))
+//console.log("modo splice:")EVITE!Pois o Splice altera a Array original.
+//console.log(listaMxConvite.splice(0,1))
+//console.log(listaTotal.splice(1,1))
+//console.log(listaFinal.splice(2,1))
+
 //- Imprima uma informação booleana verdadeira sobre a **inclusão** 
 //de um item do primeiro array, e uma informação booleana 
 //falsa sobre a **inclusão** de um item ao terceiro array. 
