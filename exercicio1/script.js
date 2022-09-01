327e5d197ba9c5e07641e2e30d7618342c5ad704
Ikebonamin <bonamin.ike@gmail.com>
2022-09-01 09:53:48 -0300

    execução dos exercícios

---
+++ exercicio1/script.js
@@ -0,0 +1,10 @@
+'strict';
+
+//a//
+let array1 = [56, 38, 45];
+//b//
+let array2 = ['joão', 'maria', 'bruxa'];
+//c//
+let array3 = [28, 'joão', true];
+//d//
+console.log(array1, array2, array3);
