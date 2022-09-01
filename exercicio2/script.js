cafc5a7323da9558fcc95a3302d76c92991ed5dd
milenarismo <milenasimoeslabenu@gmail.com>
2022-09-01 14:45:03 -0300

    Ecercícios Finalizados 010

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,1 +1,31 @@
-const minhaString = "Eu não sou supersticioso, mas sou um pouco ________.      ";
+const arrayNum = [15 , 22, 30, 42]
+const arrayStrings = ["banana", "maça", 'mamão']
+const arrayTudo = [20, "milena", false ]
+
+console.log("Quantidade de itens:", arrayNum.length)
+console.log("Quantidade de itens:", arrayStrings.length)
+console.log("Quantidade de itens:", arrayTudo.length)
+
+// const primeiroItem = arrayNum[0]
+// console.log(primeiroItem)
+
+// const segundoItem = arrayStrings[1]
+// console.log(segundoItem)
+
+// const terceiroItem = arrayTudo[2]
+// console.log(terceiroItem)
+
+console.log( arrayNum[0], arrayStrings[1], arrayTudo[2] )
+
+const inclusao1 = arrayNum.includes(30)
+console.log(inclusao1)
+
+const inclusao2 = arrayStrings.includes("gabriel")
+console.log(inclusao2)
+
+
+
+//Duas formas de fazer o item 2 do exercício!
+
+
+
