ae2ec8e7e40d264450ad822d28e0e980e694781a
Ana Marmitt <anap.mmarmitt@gmail.com>
2022-09-01 15:48:57 -0300

    ex1-feito

---
+++ exercicio1/script.js
@@ -0,0 +1,7 @@
+const arrayNum = [4, 8, 15, 16, 23, 42],
+  arrayString = ["luca", "fufuchan", "Mysta"],
+  arrayBabygirl = ["daemon", 1, true, "babygirl"];
+
+console.log(arrayNum);
+console.log(arrayString);
+console.log(arrayBla);
