7e7af6856e2a964ac60ef1cfc77331d68f59b5e1
guirozmesquita@gmail.com <guilhermer_mesquita@hotmail.com>
2022-09-01 13:03:53 -0400

    finalizando os exercicios

---
+++ exercicio1/script.js
@@ -0,0 +1,5 @@
+arrNmb = [3,6,8];
+arrStr = ['Bolacha', 'Igreja', 'Livros'];
+arrAlt = [3, 'Olá', true]
+
+console.log(arrNmb, arrStr, arrAlt)
--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,1 +1,9 @@
-const minhaString = "Eu não sou supersticioso, mas sou um pouco ________.      ";
+arrNmb = [3, 6, 8, 9, 12];
+arrStr = ['Bolacha', 'Igreja', 'Livros', 'Amigos'];
+arrAlt = [3, 'Olá', true]
+
+console.log(arrNmb.length, arrStr.length, arrAlt.length)
+console.log(arrNmb[0], arrStr[1], arrAlt[2])
+
+let arr = arrNmb.includes(true)
+console.log('O primeiro array tem o número 3? - ', arrNmb.includes(3), '|', 'O terceiro tem bolacha? - ', arrAlt.includes('Bolacha'))
