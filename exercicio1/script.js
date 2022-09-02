638933c3eef6ea6b5e72ebef607875c1d975eea6
cblouzada <minhavidaemfloripa@hotmail.com>
2022-09-01 23:54:38 -0300

    Exercicio10

---
+++ exercicio1/script.js
@@ -0,0 +1,14 @@
+// Criando a arrays de numeros.
+const numbArrays = [15, 06, 96, 03, 10, 16]
+
+// Criando apenas Strings
+const stringsArrays = ["Bolo", "Maça", "Banana" , "Cupuaçu"]
+
+// Criando, numeros, strings e booleanos
+const numStriBoo = [15, 06 ,96 , "Bolo", "Banana", "Maça", true, false]
+
+//Imprima no consele
+console.log(numbArrays, stringsArrays, numStriBoo)
+
+//Strings - É uma seguencia de caracteres(palavras)
+//Booleanos - É, (true e false)
--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,1 +1,21 @@
-const minhaString = "Eu não sou supersticioso, mas sou um pouco ________.      ";
+//Essas const foram retiradas do exercicio 1.
+// Continucação dos arrays.
+
+const numbArrays = [15, 06, 96, 03, 10, 16]
+
+const stringsArrays = ["Bolo", "Maça", "Banana" , "Cupuaçu"]
+
+const numStriBoo = [15, 06 ,96 , "Bolo", "Banana", "Maça", true, false]
+
+// Função da length para contar o tamanho. 
+// array ele conta os itens ex: 1- item bolo, 2- item banana etc; 
+// String ele conta os caracteres, ex: 1- item Bolo = 4 caracteres etc;
+
+console.log(numbArrays.length())
+console.log(stringsArrays.length())
+console.log(numStriBoo.length())
+console.log(numbArrays[0], stringsArrays[1], numStriBoo[2])
+
+console.log(numbArrays.includes(06))                                                           
+console.log(stringsArrays.includes("Jaca"))
+console.log(numStriBoo.includes(15), numStriBoo.includes("Uva")) 
