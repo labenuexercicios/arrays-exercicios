b3503c3c1668d159f64c05dcb5b94ae66a48c914
Rafaella Estecio <rafaellaestecio@gmail.com>
2022-09-01 21:27:19 -0300

    exercicio 2

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,1 +1,22 @@
-const minhaString = "Eu não sou supersticioso, mas sou um pouco ________.      ";
+
+const primeiroArray = [2,7,6,1,3,9];
+const segundoArray = ["cafe","cha","limao","bolacha"];
+const terceiroArray = [5,"banana",true,"laranja",false,9];
+const checarNumero2= primeiroArray.includes(2);
+const checarMorango= terceiroArray.includes("morango");
+
+
+
+//console.log(primeiroArray,segundoArray,terceiroArray)
+console.log(`quantidade de itens : ${primeiroArray.length}`);
+console.log(`quantidade de itens : ${segundoArray.length}`);
+console.log(`quantidade de itens : ${terceiroArray.length}`);
+
+
+
+ console.log(primeiroArray[0])
+ console.log(segundoArray[1])
+ console.log(terceiroArray[2])
+
+ console.log(`Tem numero 2? ${checarMorango}`);
+ console.log(`Tem morango? ${checarNumero2}`);
