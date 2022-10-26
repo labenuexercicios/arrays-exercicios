5c3c1a2e73a4b509682475617ac823d8ddd71ce4
Francisco-Montalvao <f.montalvao@outlook.com>
2022-10-26 15:40:42 -0300

    resolvido exercicio 2

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,1 +1,14 @@
-const minhaString = "Eu não sou supersticioso, mas sou um pouco ________.      ";
+const numeros=[94,41,12]
+const strings=["ozemela","Francisco","helany"]
+const misturados=[12,85,"Ozemela","Montalvania",2===0,7<4]
+//- Imprima a quantidade de itens de cada array (utilize um console.log() para cada impressão);
+console.log(numeros.length)
+console.log(strings.length)
+console.log(misturados.length)
+//- Imprima o primeiro item do primeiro array, o segundo item do segundo array, e o terceiro item do terceiro array;
+console.log(numeros[0])
+console.log(strings[1])
+console.log(misturados[2])
+//- Verifique se um item está incluído no primeiro array e depois no terceiro array e imprima o resultado destas verificações no console.log(). A primeira impressão deve retornar um booleano true e a segunda deve retornar um booleano false. 
+console.log(numeros.includes(94))
+console.log(misturados.includes("kenzie"))
