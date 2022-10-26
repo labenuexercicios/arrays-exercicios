ab34382fc835e5629777f842e1febf91c04282fd
WeslleiBrito <wesllei.wbs@gmail.com>
2022-10-26 16:57:17 -0300

    Exercicio 1

---
+++ exercicio1/script.js
@@ -0,0 +1,7 @@
+const numeros = [15, 101, 17, 0, 11, 21, 35]
+const letras = ["História", "Cultura", "Jornalismo", "Podcast"]
+const numerosELetrasbooleans = [70, "Nacionalidade", "Poder", false, "Educação", 92, 1005, 69, true]
+
+console.log(`Arrays de números: ${numeros}`)
+console.log(`Arrays de letras: ${letras}`)
+console.log(`Arrays de números, letras, booleanos: ${numerosELetrasbooleans}`)
