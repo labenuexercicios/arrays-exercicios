e3750959d02e28b70c2b0653b98f46130efba99b
Francisco-Montalvao <f.montalvao@outlook.com>
2022-10-26 15:27:21 -0300

    resolvido exercicio 1

---
+++ exercicio1/script.js
@@ -0,0 +1,6 @@
+const numeros=[94,41,12]
+const strings=["ozemela","Francisco","helany"]
+const misturados=[12,85,"Ozemela","Montalvania",2===0,7<4]
+console.log(numeros)
+console.log(strings)
+console.log(misturados)
