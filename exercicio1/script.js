74e246655bec6543e5b70e66c9f4317a297fe7d0
Renan <ren4n_oliveira@hotmail.com>
2022-10-26 20:45:23 -0300

    Exercicio 1

---
+++ exercicio1/script.js
@@ -0,0 +1,13 @@
+/* No primeiro exercício vamos praticar a criação de arrays. Crie 3 arrays diferentes:
+
+- O primeiro deve possuir apenas **números**, e estes números não devem seguir nenhuma ordem específica;
+- O segundo array deve possuir apenas **strings**;
+- O terceiro array deve possuir **números**, **strings** e **booleanos**.
+Imprima cada um dos arrays acima, utilizando `console.log()` */
+const arrayNum = [5,6,2,7]
+const arrayString = ["Warrior", "Mage", "Healer", "Archer"]
+const arrayRandom = [5, "Gunslinger", 20===10]
+
+console.log(arrayNum)
+console.log(arrayString)
+console.log(arrayRandom)
