6e1a1ab8ce5582b70f98a7b5fc1261e51a8683f4
Rlopesn <nogueirarln@gmail.com>
2022-10-27 11:48:11 -0300

    exercicios arrays Rafael Lopes

---
+++ exercicio1/script.js
@@ -0,0 +1,9 @@
+// exercicio 1
+
+const arrayNumb = [25, 72, 45]
+const arrayString = ["celular", "notebook", "tablet"]
+const arraySortido = [39, "Rafael", true]
+
+console.log(arrayNumb)
+console.log(arrayString)
+console.log(arraySortido)
