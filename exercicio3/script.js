b533b2740addfb55cd73e57092c29b7ef7a6ad77
reydnerguimaraes <reydnerggwp@gmail.com>
2022-10-27 18:26:16 -0300

    exercicio-reydner

---
+++ exercicio3/script.js
@@ -0,0 +1,46 @@
+const arrayNum = [50, 80, 120]
+const arrayNome = ["reydner", "Suelen", "link", "zoe"]
+const arraySortido = ["reydner", true, 20]
+
+const arrayNumCopia = [50, 80, 120]
+const arrayNomeCopia = ["reydner", "Suelen", "link", "zoe"]
+const arraySortidoCopia = ["reydner", true, 20]
+
+arrayNumCopia.push(25)
+console.log("copia", arrayNumCopia)
+
+arrayNomeCopia.pop()
+console.log("copia", arrayNomeCopia)
+
+arraySortidoCopia.splice(1,1)
+console.log(arraySortidoCopia)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
