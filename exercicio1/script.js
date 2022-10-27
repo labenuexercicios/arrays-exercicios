91a37892119edae94c06b07bfa111fd367d5b74d
garrcastro <garrcastro@gmail.com>
2022-10-27 17:27:54 -0300

    Exercício 1 feito

---
+++ exercicio1/script.js
@@ -0,0 +1,7 @@
+const numeros = [01, 15, 35, 07, 42];
+const strings = ["gabriel", "vincent", "mayana"];
+const arraymix = ["lula", 13, true, "bozo", false, 22];
+
+console.log (numeros);
+console.log(strings);
+console.log(arraymix);
