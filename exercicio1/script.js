e0a6e3c5b7c20adc1c924b01973bc406941f5e40
Tiago Leite <tiagoleite1405@gmail.com>
2022-10-27 20:12:52 -0300

    Resolver exercícios

---
+++ exercicio1/script.js
@@ -0,0 +1,7 @@
+const numArr = [5, 10, 15];
+const stringArr = ['Banana', 'Jaca', 'Pera'];
+const mixedArr = [15, 'Jaca', true];
+
+console.log(numArr);
+console.log(stringArr);
+console.log(mixedArr);
