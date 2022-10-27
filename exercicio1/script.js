7c5dd32acf5412b0c6560ca16a7ea289516dbfd6
Miguel da Luz Alves <miguelbitz@gmail.com>
2022-10-26 21:23:22 -0300

    exercicio 1 completo

---
+++ exercicio1/script.js
@@ -0,0 +1,7 @@
+const arrayNumero = [9,1,2,6,4,5,8]
+const arrayString = ["miguel", "gica", "anime"]
+const arrayVariado = ["one piece", 14, true]
+
+console.log(arrayNumero)
+console.log(arrayString)
+console.log(arrayVariado)
