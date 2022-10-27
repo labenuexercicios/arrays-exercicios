6fbc0db7a4a8ff7ab35126c953c4d17ba355b812
rafa07sr <rafa.07.g3@mail.com>
2022-10-26 21:51:34 -0300

    exercicio array corrigido Rafa

--- exercicio3/script.js
+++ exercicio3/script.js
@@ -13,18 +13,18 @@ Agora, vamos manipular os arrays, adicionando ou removendo informações. Para i
 const arrayOriginal = [5, 2, 8, 7, 6,]
 const arrayOriginalCopia = arrayOriginal.slice()
 arrayOriginalCopia.unshift(3)
-console.log(`array original`,arrayOriginal)
-console.log(`array copia`,arrayOriginalCopia)
+console.log(`array original`, arrayOriginal)
+console.log(`array copia`, arrayOriginalCopia)
 
 const array2 = [7, 9, 4, 2, 4]
 const array2Copia = array2.slice()
 array2Copia.pop()
-console.log(`array origial`,array2)
-console.log(`array copia`,array2Copia)
+console.log(`array origial`, array2)
+console.log(`array copia`, array2Copia)
 
 const array3 = [8, 5, 7, 1, 3]
 const array3Copia = array3.slice()
 array3Copia.splice(2, 1)
 
-console.log(`array original`,array3)
-console.log(`array copia`,array3Copia)
+console.log(`array original`, array3)
+console.log(`array copia`, array3Copia)
