cb8339d442b1f097a87302417ff69e2b28102681
AnaGabiM <ana.agma98@gmail.com>
2022-10-31 18:49:22 -0300

    Exercicio arrays Ozemela

---
+++ exercicio1/script.js
@@ -0,0 +1,7 @@
+const numeros = [15,23,65,42]
+const strings = ["pneu", "torta", "caneta", "mesa"]
+const aleatoria = [24,true, "celular", false]
+
+console.log(numeros)
+console.log(strings)
+console.log(aleatoria)
