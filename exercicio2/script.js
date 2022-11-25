a600bff6f55cf3025327f21245dbeb844e04179b
Mariaeduardarg <msitescriativos@gmail.com>
2022-11-25 14:06:38 -0300

    arrays-exercicio

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,1 +1,21 @@
-const minhaString = "Eu não sou supersticioso, mas sou um pouco ________.      ";
+console.log(" - parte 1 -")
+let numbers = [1, 56, 32, 263, 47]
+let strings = ["Paulo", "gato", "salmão"]
+let arrayVariado = [12, 69, 33, "trairas", "churumelas", 2>2,
+15<30, numbers ===strings]
+console.log(numbers)
+console.log(strings)
+console.log(arrayVariado)
+
+console.log(" - parte 2 -")
+console.log(numbers.length)
+console.log(strings.lenght)
+console.log(arrayVariado.length)
+
+console.log(" - parte 3 -")
+console.log(numbers.splice(0,1))
+console.log(strings.splice(1,1))
+console.log(arrayVariado.splice(2,1))
+
+console.log(numbers.includes(56))
+console.log(arrayVariado.includes("numbers"))
