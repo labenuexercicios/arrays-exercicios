ef04a1a695de8a9c48299985f88096fad9f8f252
elowuttke <elowuttke@gmail.com>
2022-11-25 11:13:57 -0300

    array exercises done

---
+++ exercicio1/script.js
@@ -0,0 +1,15 @@
+console.log(" - EXERCÍCIO 1 [ARRAYS] -")
+
+// No primeiro exercício vamos praticar a criação de arrays. Crie 3 arrays diferentes:
+// O primeiro deve possuir apenas números, e estes números não devem seguir nenhuma ordem específica;
+const arrNum = [521,63,380,17,24,187,498,36,147]
+
+// O segundo array deve possuir apenas strings;
+const arrStr = ["Brasil","batata","Maria","tesoura","filhos","suco","avós","cores"]
+
+// O terceiro array deve possuir números, strings e booleanos. Imprima cada um dos arrays acima, utilizando console.log()
+const arrMix = [248,"pneu",true,false,18,"carteira","dez",83,true]
+
+console.log(arrNum)
+console.log(arrStr)
+console.log(arrMix)
