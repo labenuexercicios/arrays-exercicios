26e663eb9441513dc237c9ccd1d468e3f31d1394
astron4ut <ctt.astro@gmail.com>
2022-12-09 21:25:57 -0300

    resolução dos exercícios

---
+++ exercicio1/script.js
@@ -0,0 +1,14 @@
+// No primeiro exercício vamos praticar a criação de arrays. Crie 3 arrays diferentes:
+
+// - O primeiro deve possuir apenas **números**, e estes números não devem seguir nenhuma ordem específica;
+// - O segundo array deve possuir apenas **strings**;
+// - O terceiro array deve possuir **números**, **strings** e **booleanos**.
+// Imprima cada um dos arrays acima, utilizando `console.log()`
+
+let arrayDeNumeros = [45, 20, 35, 65, 80];
+let arrayDeStrings = ["jessica", "maria", "fernanda"];
+let arrayDeTodoTipo = [50, 80, "johnny", "paul", true, false];
+
+console.log(arrayDeNumeros);
+console.log(arrayDeStrings);
+console.log(arrayDeTodoTipo);
