6d3344d640bce6b38d157d27117051c791ba38a2
LinconCS <linconsantos@gmail.com>
2023-02-14 20:55:17 -0300

    alteracoes

---
+++ exercicio1/script.js
@@ -0,0 +1,9 @@
+const meusNumeros = [32, 8, 729, 46, 216]
+
+const minhasStrings = ['cachorro', 'gato', 'coelho', 'ovelha', 'cavalo']
+
+const minhaListaNumerosStringsEBooleanos = ['macaco', 10, false, 348, 'camelo', true]
+
+console.log(meusNumeros)
+console.log(minhasStrings)
+console.log(minhaListaNumerosStringsEBooleanos)
