6fb9245ff32e629527ad0837572b7dd70ee6e095
LivanDoVale <livanjr4@gmail.com>
2023-02-15 09:36:49 -0300

    exercicio 1 feito

---
+++ exercicio1/script.js
@@ -0,0 +1,9 @@
+const arrayNum = [1,2,3,4]
+const arrayString = ["Livan", "Bea", "Kesz", "Samu"]
+const arrayMisto = [20, "Will", true]
+
+console.log(`
+${arrayNum}
+${arrayString}
+${arrayMisto}
+`)
