d182a27a73ae52b848ace0ac300ece65ee398821
VishLucasHenrique <lucasproenca_11@hotmail.com>
2023-03-22 09:36:18 -0300

    Arrays, Lucas Henrique Proença

---
+++ exercicio1/script.js
@@ -0,0 +1,7 @@
+const arrayNumeros = [5, 8, 16, 2, 30]
+const arrayStrings = ["Pizza", "Lucas ", "Apartamento"]
+const arrayGeral = [15, "Maria", false, 10, "Maiara", true]
+
+console.log(arrayNumeros)
+console.log(arrayStrings)
+console.log(arrayGeral)
