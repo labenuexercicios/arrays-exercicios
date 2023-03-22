7e98b447b2ff2b3d36ce4e14f1b24fb5d0ec18bf
carolkapelinski <carolkapelinkski@gmail.com>
2023-03-22 12:03:36 -0300

    exerc-arrays-resolvidos

---
+++ exercicio1/script.js
@@ -0,0 +1,19 @@
+// # Exercício 1
+
+// No primeiro exercício vamos praticar a **criação de arrays**. Crie 3 arrays diferentes:
+
+// - O primeiro deve possuir apenas **números**, e estes números não devem seguir nenhuma ordem específica;
+// - O segundo array deve possuir apenas **strings**;
+// - O terceiro array deve possuir **números**, **strings** e **booleanos**.
+
+// Imprima cada um dos arrays acima, utilizando `console.log()`
+
+const arrayNumber = [26, 29, 7, 6, 5, 4, 2]
+
+const arrayStrings = ["chimia", "agnes" , "nico", "morceguita"]
+
+const arrayNsb = [2, 800, "draculito", "edward" , false, true, true]
+
+console.log(arrayNumber)
+console.log(arrayStrings)
+console.log(arrayNsb)
