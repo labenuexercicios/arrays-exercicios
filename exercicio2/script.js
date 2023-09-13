1d966bd4b2153d61e752b66efcc21ec5cf621f96
raissanfg <99144281+raissanfg@users.noreply.github.com>
2023-09-13 14:08:14 -0500

    exercicio2-feito

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,29 +1,27 @@
 
 
 //A
-// function arrays(array1, array2, array3) {
-//   console.log(array1.length)
-//   console.log(array2.length)
-//   console.log(array3.length)
-// }
+function arrays(array1, array2, array3) {
+  console.log(array1.length)
+  console.log(array2.length)
+  console.log(array3.length)
+}
 
-// const array1 = [3,6,9,12,33,55,77]
-// const array2 = ["laptop", "celular", "televisao"]
-// const array3 = [12, "amarelo", true, 56, "azul", false]
+const array1 = [3,6,9,12,33,55,77]
+const array2 = ["laptop", "celular", "televisao"]
+const array3 = [12, "amarelo", true, 56, "azul", false]
 
-// arrays(array1, array2, array3)
+arrays(array1, array2, array3)
 
-
-//--------------------------------------------------
 
 
 
 function novosArrays(array1, array2, array3){
  
 //B    
-//   console.log(array1[0])
-//   console.log(array2[1])
-//   console.log(array3[2])
+  console.log(array1[0])
+  console.log(array2[1])
+  console.log(array3[2])
 
 //C
 console.log(array1.includes(33))
@@ -32,10 +30,6 @@ console.log(array3.includes(true))
 
 
 }
-
- const array1 = [3,6,9,12,33,55,77]
- const array2 = ["laptop", "celular", "televisao"]
- const array3 = [12, "amarelo", true, 56, "azul", false]
 
 novosArrays(array1, array2, array3)
 
