e4c2d2d5901b57bcbde78133cc5f3a6ad34ceda9
raissanfg <99144281+raissanfg@users.noreply.github.com>
2023-09-13 11:16:12 -0500

    exercicio1-feito

---
+++ exercicio1/script.js
@@ -0,0 +1,7 @@
+const array1 = [3,6,9,12,33,55,77]
+const array2 = ["laptop", "celular", "televisao"]
+const array3 = [12, "amarelo", true, 56, "azul", false]
+
+console.log(array1)
+console.log(array2)
+console.log(array3)
