fb9f87eb6e47d2c33c9c075b34cac0f22a6a5ac9
bruhoffman <bruhoffman@gmail.com>
2023-11-20 18:55:01 -0300

    Exercício 1

---
+++ exercicio1/script.js
@@ -0,0 +1,7 @@
+const arrayNumber = [4, 1, 7, 2, 8, 9, 0, 5];
+const arrayString = ["chocolate", "manga", "salada", "exercícios", "férias"];
+const arrayDiversos = ["Rio Novo do Sul", 1982, true, "trabalho", false];
+
+console.log(arrayNumber);
+console.log(arrayString);
+console.log(arrayDiversos);
